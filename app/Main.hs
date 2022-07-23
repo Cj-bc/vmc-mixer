d42ebad1f3cef01f50a60f71e99cf81de0c44770
Cj-bc <cj.bc-sd@outlook.jp>
2022-07-24 02:10:37 +0900

    Change: Use GPL-3.0 License for whole work
    As I'll use 'hosc' library, which is under GPL-3.0

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,21 @@
+{- |
+Module      :  Main
+Description :  mix multiple VMCP data and produce new data
+Copyright   :  (c) Cj.bc-sd a.k.a Cj-bc
+License     :  GPL-3
+Maintainer  :  cj.bc-sd@outlook.jp
+Stability   :  experimental
+Portability :  portable
+
+This file is part of vmc-mixer.
+
+vmc-mixer is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+vmc-mixer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with vmc-mixer. If not, see <https://www.gnu.org/licenses/>.
+-}
+
 module Main where
 
 import Lib
