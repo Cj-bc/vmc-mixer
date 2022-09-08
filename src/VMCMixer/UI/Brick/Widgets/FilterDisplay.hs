e146f7a79158bfbcb79930b3f3c228bd82ae34e3
Cj-bc <cj.bc-sd@outlook.jp>
2022-09-08 09:49:37 +0900

    Change: VMCMixer.UI.Brick.Widgets.FilterDisplay> Use 'Zipper'
    「今どの MarionetteMsgAddress に focus
    しているか」を覚えておくために Zipper に切り替えた
    Zipperを用いることで、保持したデータのうち一つを確実にピークすることが出来る。

--- src/VMCMixer/UI/Brick/Widgets/FilterDisplay.hs
+++ src/VMCMixer/UI/Brick/Widgets/FilterDisplay.hs
@@ -25,17 +25,42 @@ import Brick.Types (Widget, Padding(Pad))
 import Brick.Widgets.List (renderList, List)
 import qualified Data.HashMap.Strict as HMap
 import VMCMixer.Types (MarionetteMsgAddresses, Performer, performerName, performerPort)
+import Lens.Micro ((^.))
 import Lens.Micro.Extras (view)
 import Lens.Micro.TH (makeLenses)
 
+-- {{{ Zipper implementation
+import Lens.Micro (to, SimpleGetter)
+
+-- | Simple zipper
+data Zipper a = Zipper { _peeked :: a
+                       , _before :: [a]
+                       , _after  :: [a]
+                       }
+makeLenses  ''Zipper
+
+next :: SimpleGetter (Zipper a) (Zipper a)
+next = let getter (Zipper c bs (a:as)) = Zipper a (c:bs) as
+  in to getter
+
+-- | Return Zipper that peeks previous value
+--
+--  view (next . previous) == id
+--  view (previous . next) == id
+previous :: SimpleGetter (Zipper a) (Zipper a)
+previous = let getter (Zipper c (b:bs) as) = Zipper b bs (c:as)
+  in to getter
+
+-- }}}
+
 data FilterDisplay n = FilterDisplay { _displayName :: n
-                                     , _containedFilters :: HMap.HashMap MarionetteMsgAddresses (List n Performer)
+                                     , _containedFilters :: Zipper (MarionetteMsgAddresses, List n Performer)
                                      , _fallbackFilter :: (n, Performer)
                                      }
 makeLenses ''FilterDisplay
 
 filterDisplay :: n -> [(MarionetteMsgAddresses, List n Performer)] -> (n, Performer) -> FilterDisplay n
-filterDisplay n fs fallback = FilterDisplay n (HMap.fromList fs) fallback
+filterDisplay n (peeked':rest) fallback = FilterDisplay n (Zipper peeked' [] rest) fallback
 
 instance Named (FilterDisplay n) n where
   getName = view displayName
@@ -51,7 +76,7 @@ renderFallback fs = let (name, p) = view fallbackFilter fs
  
 renderFilterDisplay :: (Ord n, Show n) => Bool -> FilterDisplay n -> Widget n
 renderFilterDisplay isFocused map =
-        vBox $ [renderFallback map, hBorder] ++ (renderFilterInfoRow isFocused <$> (HMap.toList . view containedFilters $ map))
+        vBox $ [renderFallback map, hBorder] ++ (renderFilterInfoRow isFocused <$> mconcat [map^.containedFilters.before, [map^.containedFilters.peeked], map^.containedFilters.after])
 
 renderFilterInfoRow :: (Ord n, Show n) => Bool -> (MarionetteMsgAddresses, List n Performer) -> Widget n
 renderFilterInfoRow isFocused (addr, ls) = vBox [str $ show addr
