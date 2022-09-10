96178b3f54f630781d10ffa48ff12b822b2252a8
Cj-bc <cj.bc-sd@outlook.jp>
2022-09-10 18:44:27 +0900

    Add: test/../FilterDisplaySpec>

---
+++ test/VMCMixer/UI/Brick/Widgets/FilterDisplaySpec.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+module VMCMixer.UI.Brick.Widgets.FilterDisplaySpec where
+
+import Test.Hspec
+
+spec = do
+  describe "toFilter" $ do
+    it "should not modify contents" pending
+
+  describe "filterAdd" $ do
+    it "should add given Performer successfully" pending
+    it "should not modify fallback" pending
+    it "should not modify other address filters" pending
+
+  describe "filterRemove" $ do
+    it "should remove specified Performer successfully" pending
+    it "should not modify fallback" pending
+    it "should not modify other address filters" pending
+
+  describe "filterSetFallback" $ do
+    it "should set fallback Performer successfully" pending
+    it "should not modify others" pending
+    
+    
